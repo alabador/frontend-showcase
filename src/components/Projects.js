fca861cc5f184d953df17b928b3e128e83a88be4
alabador <austinlabador@gmail.com>
2023-03-08 02:14:36 -0800

    Update with gif and change card color bg

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,20 +5,20 @@ const Projects = () => {
     return(
         <div className='Projects'>
             <Card 
-                name = {'Four Card Feature Section'}
-                source={'./images/four-card-feature.png'} 
-                flip={'Card'}
-                project = {'https://alabador.github.io/frontend-ui/four-card-feature-section/'}
-                repo = {'https://github.com/alabador/frontend-ui/tree/main/four-card-feature-section'}
-                stack = {['HTML', 'CSS']}
-            />
-            <Card 
                 name = {'Advice Generator App'}
-                source={'./images/advice-screenshot.png'} 
-                flip={'Card-flip'}
+                source={'./images/advice-generator.gif'} 
+                flip={'Card'}
                 project = {'https://alabador.github.io/frontend-ui/advice-generator-app/'}
                 repo = {'https://github.com/alabador/frontend-ui/tree/main/advice-generator-app'} 
                 stack = {['HTML', 'SCSS', 'JavaScript']}
+            />
+            <Card 
+                name = {'Four Card Feature Section'}
+                source={'./images/four-card-feature.png'} 
+                flip={'Card-flip'}
+                project = {'https://alabador.github.io/frontend-ui/four-card-feature-section/'}
+                repo = {'https://github.com/alabador/frontend-ui/tree/main/four-card-feature-section'}
+                stack = {['HTML', 'CSS']}
             />
             <Card 
                 name = {'Altis Sign Up Form'}
