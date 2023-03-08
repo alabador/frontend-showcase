6ebed231ce6bf7464c5948e552d49832eff75c48
alabador <austinlabador@gmail.com>
2023-03-07 16:22:58 -0800

    First commit

---
+++ src/components/TopNav.js
@@ -0,0 +1,11 @@
+import { Flex, Spacer } from "@chakra-ui/react"
+
+const TopNav = () => {
+    return (
+        <Flex as="nav" w="100%" h='10' bg="red.700" textColor='white' justifyContent="center" alignItems='center'>
+            Frontend Showcase
+        </Flex>
+    )
+}
+
+export default TopNav;
