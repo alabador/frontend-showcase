4d0db54576abb28cdbb8bfeea56cfe00764bf91e
alabador <austinlabador@gmail.com>
2023-03-07 22:20:36 -0800

    Fix horizontal scrollbar using max-width

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.scss';
+import Hero from './components/Hero';
 import Projects from './components/Projects';
 
 function App() {
   return (
     <div className="App">
+      <Hero />
       <Projects />
     </div>
   );
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,7 +3,7 @@ import './Hero.scss'
 const Hero = () => {
     return (
         <div className='Hero'>
-
+            <h1>Frontend Showcase</h1>
         </div>
     )
 }
